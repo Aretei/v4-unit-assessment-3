3978a98c14090999f31bbc755ccf43179435e648
sam thompson <sammmthompson@gmail.com>
2021-03-05 16:21:21 -0700

    wanted to finish yesterdays react project first, its still a work in progress

--- classes/practice-classes.js
+++ classes/practice-classes.js
@@ -14,7 +14,20 @@
   Be sure to match the capitalization and punctuation of the string.
 */
 
-//CODE HERE
+class Character {
+  
+  constructor (name, type) {
+    this.name = name
+    this.type = type
+  }
+
+  getInfo () {
+    return `This is a ${this.type} character named ${this.name}`
+  }
+
+}
+
+const clank = new Character('Clank', 'robot')
 
 //////////////////PROBLEM 2////////////////////
 
@@ -32,14 +45,26 @@
   Call your new class NPC
 */
 
-//CODE HERE
+class NPC extends Character {
+    constructor (name, type, location, phrase) {
+      super(name, type, 'NPC')
+      this.location = location
+      this.phrase = phrase
+    }
+    dialogue () {
+      return `${this.name}: ${this.phrase}`
+    }
+    getInfo () {
+      super.getInfo()
+    }
+}
 
 /*
     Create an NPC named Ralph who is a human located in Niceland. His phrase should be `I'm gonna wreck it!`. 
     Store your new NPC in a variable called 'ralph'.
 */
 
-//CODE HERE
+const ralph = new NPC('Ralph', 'human', 'Niceland', "I'm gonna wreck it!")
 
 /*
     Next you're going to create three variables to store information about Ralph.
@@ -48,7 +73,10 @@
     Third, make a variable named ralphsLocation whose value will be Ralph's location.
 */
 
-//CODE HERE
+let ralphsInfo = ralph.getInfo()
+let ralphsDialogue = ralph.dialogue()
+let ralphsLocation = ralph.location
+console.log(ralph.getInfo())
 
 //////////////////PROBLEM 3////////////////////
 
@@ -73,7 +101,27 @@
   Call your new class Player
 */
 
-//CODE HERE
+class Player extends Character {
+    constructor (name, type, healthLevel, attackLevel) {
+      super(name, type, 'Player')
+      this.healthLevel = healthLevel
+      this.attackLevel = attackLevel
+    }
+    defend (attackNum) {
+      this.healthLevel = this.healthLevel - attackNum
+
+      if (this.healthLevel > 0) {
+        return {
+          attackStrength: attackNum,
+          remainingHealth: this.healthLevel,
+          message: `${this.name} is still in the fight!`
+        } 
+      }
+      else {
+        return `${this.name} has been defeated!`
+      }
+    }
+}
 
 /*
     Next, we'll create two Players.
@@ -83,7 +131,8 @@
     and he's a firebender type with a 100 healthLevel and 0 attackLevel.
 */
 
-//CODE HERE
+let aang = new Player('Aang', 'airbender', 100, 100)
+let ozai = new Player('Ozai', 'firebender', 100, 0)
 
 /*
     Let's see how a fight between these two would go. 
@@ -92,7 +141,7 @@
     (You can console log battle to see what happens)
 */
 
-//CODE HERE
+let battle = ozai.defend(100)
 
 //////////////////PROBLEM 4////////////////////
 
@@ -110,7 +159,18 @@
       - Example string: `Wonder Woman used flight!`
 */
 
-//CODE HERE
+class Hero extends Player {
+  constructor (name, type, healthLevel, attackLevel, superPowers) {
+    super(name, type, healthLevel, attackLevel, 'Hero')
+    this.superPowers = []
+  }
+  addSuperPower (newPower) {
+    this.superPowers.push(newPower)
+  }
+  useSuperPower (index) {
+    return `${this.name} used ${this.superPowers[index]}!`
+  }
+}
 
 /*
   Create a hero named 'Fire Spitter' whose type is 'dragon'. 
@@ -121,4 +181,14 @@
   Last, invoke useSuperPower passing in 0 for the index and store the result in a variable called fireSpitterAttack.
 */
 
-//CODE HERE
+let fireSpitter = new Hero('Fire Spitter', 'dragon', 5000, 5000)
+
+fireSpitter.addSuperPower('spitting fire')
+fireSpitter.addSuperPower('flight')
+fireSpitter.addSuperPower('giant freaking lizard')
+
+console.log(fireSpitter.superPowers)
+
+
+console.log(fireSpitter.useSuperPower(0))
+
--- closures/practice-closures.js
+++ closures/practice-closures.js
@@ -8,12 +8,22 @@
     myFunc should return getSecretString
   */
   
-  //CODE HERE
+  function myFunc () {
+    let myStr = 'super secret string'
+    function getSecretString () {
+      return myStr
+    }
+
+    return getSecretString()
+   
+  }
+
+  console.log(myFunc())
 
   
   //Now create a variable called secretString. Its value should be the invocation of myFunc.
 
-  //CODE HERE
+let secretString = myFunc()
   
   
   ////////////PROBLEM 2////////////
@@ -28,27 +38,47 @@
     The lightSwitch function should return flipTheSwitch.
   */
   
-  //CODE HERE
+  function lightSwitch () {
+    let isTheLightOn = false
+
+    function flipTheSwitch () {
+      if (isTheLightOn === false) {
+        isTheLightOn = true
+        console.log(isTheLightOn)
+        return 'The light is on'
+      }
+      else if (isTheLightOn === true) {
+        isTheLightOn = false
+        console.log(isTheLightOn)
+        return 'The light is off'
+      }
+    }
+
+    return flipTheSwitch()
+  }
 
   
   //Create a variable called kitchenSwitch whose value is the invocation of lightSwitch.
   
-  //CODE HERE
+ let kitchenSwitch = lightSwitch()
 
   
   //Invoke kitchenSwitch.
 
-  //CODE HERE
+kitchenSwitch
+
   
   
   //Create a variable called bathroomSwitch whose value is the invocation of lightSwitch. 
 
-  //CODE HERE
+  let bathroomSwitch = lightSwitch()
   
   
   //Invoke bathroomSwitch twice.
   
-  //CODE HERE
+  bathroomSwitch
+  bathroomSwitch
+
 
   
   
@@ -63,7 +93,24 @@
       - 'prunePlant' should subtract 1 from the height and return the new height
   */
 
-  //CODE HERE
+  function plantTracker () {
+    plant = 'fern'
+    height = 12
+
+    return {
+      readInfo: function () {
+        return `This is a ${plant} plant that is ${height} inches tall.`
+      },
+      waterPlant: function () {
+        height = height + 1
+        return height
+      },
+      prunePlant: function () {
+        height = height - 1
+        return height
+      }
+    }
+  }
 
 
   ////////////PROBLEM 4////////////
@@ -78,19 +125,44 @@
         - hint: try to find the index of the string first
   */
 
-  //CODE HERE
+  function inventory () {
+    products = []
+
+    return {
+      readProducts: function () {
+        return products
+      },
+      addToProducts: function (item) {
+        products.push(item)
+        console.log(products)
+        return products
+      },
+      deleteFromProducts: function (item) {
+        for (let i = 0; i < products.length; i++) {
+          if (products[i] === item) {
+            products.splice(i, 1)
+          }
+          else {
+            //pog
+          }
+
+        }
+      }
+    }
+  }
 
 
   /*
     Create a variable called 'shoes' whose value is the invocation of inventory.
   */
 
-  //CODE HERE
+  let shoes = inventory()
 
 
   /*
     Add an item to your shoes array using the addToProducts function
   */
 
-  //CODE HERE
+ shoes.addToProducts('nike')
+ 
 
